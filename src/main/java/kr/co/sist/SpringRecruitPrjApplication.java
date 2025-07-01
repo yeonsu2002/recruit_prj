ed52442b775f2cb9a2ccc48359ec6005a98d929e
githyunsuk <wngustjr1306@naver.com>
2025-07-01 15:25:36 +0900

    first commit

---
+++ src/main/java/kr/co/sist/SpringRecruitPrjApplication.java
@@ -0,0 +1,13 @@
+package kr.co.sist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecruitPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecruitPrjApplication.class, args);
+	}
+
+}
