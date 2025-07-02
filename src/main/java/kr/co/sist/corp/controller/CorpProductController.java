7b67873829a419562793a7830c8f032ada7f05d9
mingi_Shin <shinmingi0621@gmail.com>
2025-07-02 17:59:39 +0900

    0702

--- src/main/java/kr/co/sist/corp/controller/CorpProductController.java
+++ src/main/java/kr/co/sist/corp/controller/CorpProductController.java
@@ -1,5 +1,13 @@
 package kr.co.sist.corp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CorpProductController {
 
+	@GetMapping("/goCorpInfoPage")
+	public String getCorpInfoPage() {
+		return "corp/mingi/getCorpInfo";
+	}
 }
