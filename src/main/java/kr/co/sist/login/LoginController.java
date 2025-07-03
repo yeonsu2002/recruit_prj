31a046f803c876393d697e0c888ddf20d68e12d0
mingi_Shin <shinmingi0621@gmail.com>
2025-07-04 01:15:21 +0900

    0704 1:15 졸리다

--- src/main/java/kr/co/sist/login/LoginController.java
+++ src/main/java/kr/co/sist/login/LoginController.java
@@ -2,6 +2,7 @@ package kr.co.sist.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -72,4 +73,28 @@ public class LoginController {
     return "login/joinCorpForm";
   }
   
+  /**
+   * 일반회원 가입 프로세스
+   */
+  @PostMapping("/user/joinProcess")
+	public String userJoinProcess(/* @ModelAttribute UserDTO userDTO */) {
+		/* loginJoinService.registerUser(userDTO); */
+  	
+  	return "redirect:/user/login";
+  }
+  
+  /**
+   * 기업회원 가입 프로세스
+   */
+  @PostMapping("/corp/joinProcess")
+	public String corpJoinProcess(/* @ModelAttribute TotalCorpDTO totalCorpDTO */) {
+  	//통합DTO로 받고, Service에서 분리시켜 각 Entity생성
+		/* loginJoinService.registerCorp(totalCorpDTO); */
+  	
+  	return "redirect:/user/login";
+  }
+  
+  /**
+   * 이메일 중복 체크
+   */
 }
