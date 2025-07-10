2203b57adfb95b12be3e5c4df039db5fd1eb8076
mingi_Shin <shinmingi0621@gmail.com>
2025-07-10 19:27:13 +0900

    7:27

---
+++ src/main/java/kr/co/sist/error/errorController.java
@@ -0,0 +1,13 @@
+package kr.co.sist.error;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class errorController {
+
+	@GetMapping("/accessDenied")
+	public String errorPage() {
+		return "error/accessDenied";
+	}
+}
