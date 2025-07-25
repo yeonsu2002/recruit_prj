558d2988f2e56e802a865f9a168ba81a32ddc9bb
jjn841 <nyeong1236@gamail.com>
2025-07-25 16:02:32 +0900

    장준녕 커밋

--- src/main/java/kr/co/sist/corp/controller/ResumeDetailController.java
+++ src/main/java/kr/co/sist/corp/controller/ResumeDetailController.java
@@ -1,12 +1,14 @@
 package kr.co.sist.corp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.sist.corp.dto.ResumeDetailDTO;
 import kr.co.sist.corp.service.ResumeDetailService;
 import kr.co.sist.corp.service.TalentPoolService;
 import kr.co.sist.jwt.CustomUser;
@@ -18,27 +20,29 @@ public class ResumeDetailController {
     private final ResumeDetailService resumeDetailService;
     private final TalentPoolService talentPoolService;
 
-    @Autowired
     public ResumeDetailController(ResumeDetailService resumeDetailService,
                                    TalentPoolService talentPoolService) {
         this.resumeDetailService = resumeDetailService;
         this.talentPoolService = talentPoolService;
     }
 
-    @GetMapping("/{resumeId}")
-    public ResumeDetailDTO getResumeDetail(@PathVariable("resumeId") Long resumeId) {
-        return resumeDetailService.getResumeDetail(resumeId);
-    }
+//    @GetMapping("/{resumeId}")
+//    public ResumeDetailDTO getResumeDetail(@PathVariable("resumeId") Long resumeId) {
+//        return resumeDetailService.getResumeDetail(resumeId);
+//    }
 
     @PostMapping("/resume/view")
     public ResponseEntity<?> viewResume(@RequestParam("resumeSeq") Long resumeSeq,
-                                        @AuthenticationPrincipal CustomUser corpInfo) {
+                                        @AuthenticationPrincipal CustomUser corpInfo,Model model) {
         try {
             Long corpNo = corpInfo.getCorpNo();
-
+            String corpName = corpInfo.getName();
             // 이력서 열람 기록 + 알림 메일 전송 처리
             talentPoolService.viewResume(resumeSeq, corpNo);
-
+            
+            // 이름을 모델에 추가하여 Thymeleaf 템플릿에서 사용할 수 있도록 함
+            model.addAttribute("corpName", corpName);
+            
             return ResponseEntity.ok().body("success");
         } catch (Exception e) {
             e.printStackTrace(); // 디버깅 시 원인 파악에 도움
